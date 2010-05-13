9a9d82e24409026a6d97d64d15415d646289b5ff
Clint Morgan <clint.a.m@gmail.com>
2010-05-13 12:23:51 -0700

    Initial commit of transactional contrib from hbase. This comes from the 0.20 branch, with some fixes that had not been commited. It has been reworked to compile against hbase trunk.

---
+++ src/main/java/org/apache/hadoop/hbase/client/tableindexed/IndexedTableAdmin.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright 2009 The Apache Software Foundation Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.apache.hadoop.hbase.client.tableindexed;
+
+import java.io.IOException;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.ColumnNameParseException;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.regionserver.tableindexed.IndexMaintenanceUtils;
+import org.apache.hadoop.hbase.util.Bytes;
+
+/**
+ * Extension of HBaseAdmin that creates indexed tables.
+ */
+public class IndexedTableAdmin extends HBaseAdmin {
+
+    private static final Log LOG = LogFactory.getLog(IndexedTableAdmin.class);
+
+    /**
+     * Constructor
+     * 
+     * @param conf Configuration object
+     * @throws MasterNotRunningException
+     */
+    public IndexedTableAdmin(final HBaseConfiguration conf) throws MasterNotRunningException {
+        super(conf);
+    }
+
+    /**
+     * Creates a new indexed table
+     * 
+     * @param desc table descriptor for table
+     * @throws IOException
+     */
+    public void createIndexedTable(final IndexedTableDescriptor desc) throws IOException {
+        super.createTable(desc.getBaseTableDescriptor());
+        this.createIndexTables(desc);
+    }
+
+    private void createIndexTables(final IndexedTableDescriptor indexDesc) throws IOException {
+        byte[] baseTableName = indexDesc.getBaseTableDescriptor().getName();
+        for (IndexSpecification indexSpec : indexDesc.getIndexes()) {
+            HTableDescriptor indexTableDesc = createIndexTableDesc(baseTableName, indexSpec);
+            super.createTable(indexTableDesc);
+        }
+    }
+
+    private HTableDescriptor createIndexTableDesc(final byte[] baseTableName, final IndexSpecification indexSpec)
+            throws ColumnNameParseException {
+        HTableDescriptor indexTableDesc = new HTableDescriptor(indexSpec.getIndexedTableName(baseTableName));
+        Set<byte[]> families = new TreeSet<byte[]>(Bytes.BYTES_COMPARATOR);
+        families.add(IndexedTable.INDEX_COL_FAMILY);
+        for (byte[] column : indexSpec.getAllColumns()) {
+            families.add(Bytes.add(KeyValue.parseColumn(column)[0], new byte[] {
+                KeyValue.COLUMN_FAMILY_DELIMITER
+            }));
+        }
+
+        for (byte[] colFamily : families) {
+            indexTableDesc.addFamily(new HColumnDescriptor(colFamily));
+        }
+
+        return indexTableDesc;
+    }
+
+    /**
+     * Remove an index for a table.
+     * 
+     * @throws IOException
+     */
+    public void removeIndex(final byte[] baseTableName, final String indexId) throws IOException {
+        super.disableTable(baseTableName);
+        HTableDescriptor desc = super.getTableDescriptor(baseTableName);
+        IndexedTableDescriptor indexDesc = new IndexedTableDescriptor(desc);
+        IndexSpecification spec = indexDesc.getIndex(indexId);
+        indexDesc.removeIndex(indexId);
+        this.disableTable(spec.getIndexedTableName(baseTableName));
+        this.deleteTable(spec.getIndexedTableName(baseTableName));
+        super.modifyTable(baseTableName, desc);
+        super.enableTable(baseTableName);
+    }
+
+    /** Add an index to a table. */
+    public void addIndex(final byte[] baseTableName, final IndexSpecification indexSpec) throws IOException {
+        LOG.warn("Adding index to existing table [" + Bytes.toString(baseTableName) + "], this may take a long time");
+        // TODO, make table read-only
+        LOG.warn("Not putting table in readonly, if its being written to, the index may get out of sync");
+        HTableDescriptor indexTableDesc = createIndexTableDesc(baseTableName, indexSpec);
+        super.createTable(indexTableDesc);
+        super.disableTable(baseTableName);
+        IndexedTableDescriptor indexDesc = new IndexedTableDescriptor(super.getTableDescriptor(baseTableName));
+        indexDesc.addIndex(indexSpec);
+        super.modifyTable(baseTableName, indexDesc.getBaseTableDescriptor());
+        super.enableTable(baseTableName);
+        reIndexTable(baseTableName, indexSpec);
+    }
+
+    private void reIndexTable(final byte[] baseTableName, final IndexSpecification indexSpec) throws IOException {
+        HTable baseTable = new HTable(baseTableName);
+        HTable indexTable = new HTable(indexSpec.getIndexedTableName(baseTableName));
+        Scan scan = new Scan();
+        scan.addColumns(indexSpec.getAllColumns());
+        for (Result rowResult : baseTable.getScanner(scan)) {
+            SortedMap<byte[], byte[]> columnValues = new TreeMap<byte[], byte[]>(Bytes.BYTES_COMPARATOR);
+            for (Entry<byte[], NavigableMap<byte[], byte[]>> familyEntry : rowResult.getNoVersionMap().entrySet()) {
+                for (Entry<byte[], byte[]> cellEntry : familyEntry.getValue().entrySet()) {
+                    columnValues.put(Bytes.add(familyEntry.getKey(), Bytes.toBytes(":"), cellEntry.getKey()), cellEntry
+                            .getValue());
+                }
+            }
+            if (IndexMaintenanceUtils.doesApplyToIndex(indexSpec, columnValues)) {
+                Put indexUpdate = IndexMaintenanceUtils.createIndexUpdate(indexSpec, rowResult.getRow(), columnValues);
+                indexTable.put(indexUpdate);
+            }
+        }
+    }
+}
---
+++ src/main/java/org/apache/hadoop/hbase/regionserver/transactional/THLog.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2009 The Apache Software Foundation Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.apache.hadoop.hbase.regionserver.transactional;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.regionserver.wal.HLog;
+import org.apache.hadoop.hbase.regionserver.wal.LogRollListener;
+import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+
+/**
+ * Add support for transactional operations to the regionserver's write-ahead-log.
+ */
+class THLog extends HLog {
+
+    public THLog(final FileSystem fs, final Path dir, final Path oldLogDir, final HBaseConfiguration conf,
+            final LogRollListener listener) throws IOException {
+        super(fs, dir, oldLogDir, conf, listener);
+    }
+
+    // @Override
+    // protected SequenceFile.Writer createWriter(Path path) throws IOException {
+    // return super.createWriter(path, THLogKey.class, WALEdit.class);
+    // }
+    //
+    // @Override
+    // protected HLogKey makeKey(byte[] regionName, byte[] tableName, long seqNum, long now) {
+    // return new THLogKey(regionName, tableName, seqNum, now);
+    // }
+
+    public void writeUpdateToLog(final HRegionInfo regionInfo, final long transactionId, final Put update)
+            throws IOException {
+        this.append(regionInfo, update, transactionId);
+    }
+
+    public void writeDeleteToLog(final HRegionInfo regionInfo, final long transactionId, final Delete delete)
+            throws IOException {
+        this.append(regionInfo, delete, transactionId);
+    }
+
+    public void writeCommitToLog(final HRegionInfo regionInfo, final long transactionId) throws IOException {
+        this.append(regionInfo, System.currentTimeMillis(), THLogKey.TrxOp.COMMIT, transactionId);
+    }
+
+    public void writeAbortToLog(final HRegionInfo regionInfo, final long transactionId) throws IOException {
+        this.append(regionInfo, System.currentTimeMillis(), THLogKey.TrxOp.ABORT, transactionId);
+    }
+
+    /**
+     * Write a general transaction op to the log. This covers: start, commit, and abort.
+     * 
+     * @param regionInfo
+     * @param now
+     * @param txOp
+     * @param transactionId
+     * @throws IOException
+     */
+    public void append(final HRegionInfo regionInfo, final long now, final THLogKey.TrxOp txOp, final long transactionId)
+            throws IOException {
+        THLogKey key = new THLogKey(regionInfo.getRegionName(), regionInfo.getTableDesc().getName(), -1, now, txOp,
+                transactionId);
+        WALEdit e = new WALEdit();
+        e.add(new KeyValue(new byte[0], 0, 0)); // Empty KeyValue
+        super.append(regionInfo, e, now, regionInfo.isMetaRegion());
+    }
+
+    /**
+     * Write a transactional update to the log.
+     * 
+     * @param regionInfo
+     * @param update
+     * @param transactionId
+     * @throws IOException
+     */
+    public void append(final HRegionInfo regionInfo, final Put update, final long transactionId) throws IOException {
+
+        long commitTime = System.currentTimeMillis();
+
+        THLogKey key = new THLogKey(regionInfo.getRegionName(), regionInfo.getTableDesc().getName(), -1, commitTime,
+                THLogKey.TrxOp.OP, transactionId);
+
+        for (KeyValue value : convertToKeyValues(update)) {
+            WALEdit e = new WALEdit();
+            e.add(value);
+            super.append(regionInfo, e, commitTime, regionInfo.isMetaRegion());
+        }
+    }
+
+    /**
+     * Write a transactional delete to the log.
+     * 
+     * @param regionInfo
+     * @param delete
+     * @param transactionId
+     * @throws IOException
+     */
+    public void append(final HRegionInfo regionInfo, final Delete delete, final long transactionId) throws IOException {
+
+        long commitTime = System.currentTimeMillis();
+
+        THLogKey key = new THLogKey(regionInfo.getRegionName(), regionInfo.getTableDesc().getName(), -1, commitTime,
+                THLogKey.TrxOp.OP, transactionId);
+
+        for (KeyValue value : convertToKeyValues(delete)) {
+            WALEdit e = new WALEdit();
+            e.add(value);
+            super.append(regionInfo, e, commitTime, regionInfo.isMetaRegion());
+        }
+    }
+
+    private List<KeyValue> convertToKeyValues(final Put update) {
+        List<KeyValue> edits = new ArrayList<KeyValue>();
+
+        for (List<KeyValue> kvs : update.getFamilyMap().values()) {
+            for (KeyValue kv : kvs) {
+                edits.add(kv);
+            }
+        }
+        return edits;
+    }
+
+    private List<KeyValue> convertToKeyValues(final Delete delete) {
+        List<KeyValue> edits = new ArrayList<KeyValue>();
+
+        for (List<KeyValue> kvs : delete.getFamilyMap().values()) {
+            for (KeyValue kv : kvs) {
+                edits.add(kv);
+            }
+        }
+        return edits;
+    }
+}
